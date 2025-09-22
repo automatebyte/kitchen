b7e27696b6ed6d4fa34cfb73adae99cdc3aafbfd
bethwelkipruto <bethwel.c7@gmail.com>
2025-09-22 22:20:01 +0300

    chore: import auth utilities and models for user and order handling

--- server/app.py
+++ server/app.py
@@ -9,6 +9,10 @@ from flask_restful import Resource
 # Local imports
 from config import app, db, api
 # Add your model imports
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+from models import User, Order, OrderItem
+
 
 
 # Views go here!
