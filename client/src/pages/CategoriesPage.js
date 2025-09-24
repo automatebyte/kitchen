165bfe6aaa22bc23092bc27a18e9fbe3a6dc1ad4
bethwelkipruto <bethwel.c7@gmail.com>
2025-09-24 10:15:25 +0300

    Create CategoriesPage with category management

---
+++ client/src/pages/CategoriesPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CategoriesList from '../components/Categories/CategoriesList';
+
+function CategoriesPage() {
+  return (
+    <div className="categories-page">
+      <CategoriesList />
+    </div>
+  );
+}
+
+export default CategoriesPage;
