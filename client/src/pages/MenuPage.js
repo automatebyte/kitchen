9a7e927b755e6d1c698b56772beaa459fb824617
bethwelkipruto <bethwel.c7@gmail.com>
2025-09-24 10:15:14 +0300

    Create MenuPage with menu display

---
+++ client/src/pages/MenuPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MenuList from '../components/Menu/MenuList';
+
+function MenuPage() {
+  return (
+    <div className="menu-page">
+      <MenuList />
+    </div>
+  );
+}
+
+export default MenuPage;
